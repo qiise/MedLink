7ac7f62cd257ea2b59e5402e72c6256868f22fef
Grace Li <grace.li2@columbia.edu>
2025-02-08 21:12:31 -0500

    merged frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Forum from "./pages/Forum";
+import Profiles from "./pages/Profiles";
 import MentorRequest from "./pages/MentorRequest";
-import PostDetail from "./pages/PostDetail";
+import NotFound from "./pages/NotFound";
+import PostDetail from "./pages/PostDetail"; // Import the PostDetail component
 
 const queryClient = new QueryClient();
 
@@ -17,13 +21,21 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/mentor-request" element={<MentorRequest />} />
-          <Route path="/post/:id" element={<PostDetail />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div className="min-h-screen">
+          <NavBar />
+          <div className="pt-16">
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/forum" element={<Forum />} />
+              <Route path="/profiles" element={<Profiles />} />
+              <Route path="/post/:id" element={<PostDetail />} /> {/* Add route for post details */}
+              <Route path="/mentor-request" element={<MentorRequest />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </div>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
